f3bdbab1db2c2d33f7a77ef0b223f4b08ff83c86
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-11-16 14:34:24 -0300

    :construction: redefinindo estruturas

--- router.h
+++ router.h
@@ -9,31 +9,45 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-#define TIMEOUT_MAX 10    //tempo maximo timeout
-#define TIMEOUT_MS 100
-#define N_ROT 4       //número de roteadores
-#define MSG_SIZE 100  //tamanho da mensagem
-#define QUEUE_SIZE 100//tamanho da fila dos roteadores
+#define TIMEOUT_MAX 10		//Quantidade Maxima de Reenvios
+#define TIMEOUT_MS 100		//Tempo timeout
+#define N_ROT 4				//número de roteadores
+#define MSG_SIZE 100		//tamanho da mensagem
+#define QUEUE_SIZE 100		//tamanho da fila dos roteadores
 #define TRUE 1
 #define FALSE 0
 #define INFINITE 999999
 
-typedef struct{       //estrutura do pacote
-    int origin, dest, type;
-    unsigned short ack;
-    int num_pack;     //número do pacote
-    int dist[N_ROT]; 
-    char content[MSG_SIZE]; //conteudo da mensagem
-}Package;
+typedef struct{				//Estrutura do Cabeçalho
+	int type;				//Tipo do pacote
+	int num_pack;			//número do pacote
+	int origin, dest;		//Origem e Destino
+}Packet_Header;
 
-typedef struct router{//estrutura do roteador
-     int id, port, waiting_ack;
-     Package msg_in[QUEUE_SIZE], msg_out[QUEUE_SIZE]; //filas de entrada e saída
-     char ip[32];
+typedef struct{				//Pacotes de Dados
+	Packet_Header header; 	//Cabeçalho
+	char message[MSG_SIZE];	//Mensagem de dados
+}Data_Packet;
+
+typedef struct{   			//Pacote de Configuração
+	Packet_Header header; 	//Cabeçalho
+	int dists_cost[N_ROT]; 	//Vetor Distancia
+}Config_Packet;
+
+typedef struct{   			//Pacote de Confirmação
+	Packet_Header header; 	//Cabeçalho
+	int num_confirm_pack;  	//Número do pacote que foi confirmado
+}Ack_Packet;
+
+typedef struct{   			//Estrutura de cada roteador
+	int id;         		//Identificador do roteador
+	int port;       		//Porta do roteador
+	int num_waiting_ack;   	//Número do pacote que está esperando confirmação
+	short waiting_ack;     	//Booleana ack
+	char ip[32];    		//Endereço IP do roteador
 }Router;
 
-typedef struct{       //Estrutura das tabelas de roteamentos
-  int cost[N_ROT];    //custo
-  int path[N_ROT];    //caminho
-  int conex[N_ROT];
+typedef struct{				//Estrutura da tabela de roteamento
+	int cost[N_ROT];		//Custo até o destino
+	int path[N_ROT];		//Próximo Salto
 }Table;
